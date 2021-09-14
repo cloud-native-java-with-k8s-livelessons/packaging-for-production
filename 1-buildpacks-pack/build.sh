19d1424cc6a57880096add1c26080509f11e8fc9
Josh Long <josh@joshlong.com>
2021-09-14 15:35:27 -0700

    commit

--- 1-buildpacks-pack/build.sh
+++ 1-buildpacks-pack/build.sh
@@ -6,5 +6,6 @@ PREFIX=cnj
 IMAGE=buildpacks-pack
 IMAGE_NAME=$PREFIX/$IMAGE
 
+pack config default-builder paketobuildpacks/builder:base
 pack build $IMAGE_NAME --path .
 docker run  -p 8080:8080 -e SERVER_PORT=8080  $IMAGE_NAME
